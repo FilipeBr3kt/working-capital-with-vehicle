d2998038b926e90631d8412a16c152a77a12ab07
Filipe <filipe@evosix.com.br>
2024-11-12 15:57:03 -0300

    feat: create common responses

---
+++ src/ApiGiroComVeiculo.Api/Common/Responses/StoredProcedures.cs
@@ -0,0 +1,23 @@
+﻿namespace ApiGiroComVeiculo.Api.Common.Responses;
+
+public class ValidateProtocolProcedureResponse
+{
+    public int Id { get; set; }
+    public string? NrProtocolo { get; set; }
+    public string? Nome { get; set; }
+    public bool BlAtendimento { get; set; }
+    public int UltDias { get; set; }
+    public byte DmStatus { get; set; }
+    public byte DmSituacao { get; set; }
+    public bool Result { get; set; }
+    public string? Msg { get; set; }
+}
+
+public class InsertProtocolProcedureResponse
+{
+    public int IdProtocolo { get; set; }
+    public string? NrProtocolo { get; set; }
+    public string? Senha { get; set; }
+    public bool Result { get; set; }
+    public string? Msg { get; set; }
+}
---
+++ src/ApiGiroComVeiculo.Api/Common/Responses/Views.cs
@@ -0,0 +1,17 @@
+﻿namespace ApiGiroComVeiculo.Api.Common.Responses;
+
+public class GetProtocolResponseView
+{
+    public int Id { get; set; }
+    public string NrProtocol { get; set; } = null!;
+    public DateTime DateRegister { get; set; }
+    public string Document { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public int PhoneDdd { get; set; }
+    public string PhoneNumber { get; set; } = null!;
+    public int RequestedValue { get; set; }
+    public string Plate { get; set; } = null!;
+    public int Type { get; set; }
+    public int Status { get; set; }
+    public int Situation { get; set; }
+}
