152396c0e9d23fc568ee905bab8d57bb158ee1c3
Filipe <filipe@evosix.com.br>
2024-11-12 15:56:29 -0300

    feat: create dtos

---
+++ src/ApiGiroComVeiculo.Api/Common/DTOs/StoredProcedures.cs
@@ -0,0 +1,56 @@
+﻿namespace ApiGiroComVeiculo.Api.Common.DTOs;
+
+public record VerifyUserInformationDTO(string Cnpj, string Plate);
+
+public record InsertProtocolDTO(
+    // Proposal
+    int? SessionId,
+    int? TermId,
+    int? IndicationId,
+    int? ReasonId,
+    int? PartnerId,
+    int? StoreId,
+    int? ChannelId,
+    string ReasonDescription,
+
+    // Customer
+    string CustomerCNPJ,
+    string CustomerName,
+    string CustomerFantasyName,
+    int CustomerInvoiceAmount,
+    int CustomerBusinessSizeStatus,
+
+    // Partner
+    string PartnerName,
+    string PartnerCPF,
+
+    // Contact
+    string ContactName,
+    string ContactEmail,
+    int ContactPhoneDdd,
+    string ContactPhoneNumber,
+
+    // Vehicle
+    string VehiclePlate,
+    int VehicleModelYear,
+    int VehicleManufactureYear,
+    int VehicleMarketValue,
+    string VehicleBrand,
+    string VehicleModel,
+    int VehicleTypeId,
+    int IsVehicleInCustomerName,
+    int IsVehicleFullyPaid,
+
+    // Operation
+    int RequestedAmount,
+    int TermDuration,
+    int IsAcceptanceId,
+    int IsAcceptanceTermId,
+    DateTime AcceptanceDate,
+    DateTime TermStartDate,
+    DateTime TermEndDate,
+    DateTime RegistrationStartDate,
+    DateTime RegistrationEndDate,
+    string IpAddress
+);
+
